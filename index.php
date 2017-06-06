9000a724990d29840089257a38a058130532fff3
German <elmaildegerman@gmail.com>
2017-06-06 04:34:11 +0000

    se fue

--- index.php
+++ index.php
@@ -4,12 +4,17 @@ require_once "vendor/autoload.php";
 
 $filesArr = scandir('/home/ubuntu/workspace');
 
-array_map(function($file) {
-				
-	echo "<a href='/{$file}'> {$file} </a> <br/>";
-						
-}, $filesArr);
+array_filter($filesArr, function($file) {
 
+	//if(strpos($file, '.php') ){
+
+    	echo "<a href='/{$file}'> {$file} </a> <br/>";
+
+	//}
+			    
+});
+
+var_dump($res);
 
 
 ?>
